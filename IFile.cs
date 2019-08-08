fb72f29663a38174f6927e7067b715249547bc4d
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:00:57 -0700

    Add files via upload

---
+++ IFile.cs
@@ -0,0 +1,22 @@
+﻿namespace Penguin.Files.Abstractions
+{
+    /// <summary>
+    /// A simple interface for passing around a file
+    /// </summary>
+    public interface IFile
+    {
+        #region Properties
+
+        /// <summary>
+        /// A byte array representing the file contents
+        /// </summary>
+        byte[] Data { get; }
+
+        /// <summary>
+        /// The full path to the file, or full name for the file
+        /// </summary>
+        string FileName { get; }
+
+        #endregion Properties
+    }
+}
